dc57b0ce56e43d465a0fb5c25852363c30acfb07
Kei Ito <kei.itof@gmail.com>
2023-08-16 23:09:18 +0900

    style: add .prettierrc

--- index.mjs
+++ index.mjs
@@ -6,155 +6,174 @@
 /** @typedef {{}} Node */
 /** @typedef {{create: (context: Context) => {[nodeType: string]: (node: Node) => void}}} RuleModule */
 
-const reservedIdentifiers = new Set(['undefined', 'Infinity', 'NaN', 'true', 'false']);
+const reservedIdentifiers = new Set([
+  'undefined',
+  'Infinity',
+  'NaN',
+  'true',
+  'false',
+]);
 const isNonVariableIdentifier = (node) => {
-    if (reservedIdentifiers.has(node.name)) {
-        return true;
-    }
-    const {parent} = node;
-    switch (parent && parent.type) {
+  if (reservedIdentifiers.has(node.name)) {
+    return true;
+  }
+  const { parent } = node;
+  switch (parent && parent.type) {
     case 'TSMethodSignature':
-        return parent.key === node || parent.params.includes(node);
+      return parent.key === node || parent.params.includes(node);
     case 'PropertyDefinition':
     case 'Property':
     case 'ClassProperty':
     case 'MethodDefinition':
     case 'TSPropertySignature':
-        return parent.key === node;
+      return parent.key === node;
     case 'TSTypeParameter':
-        return parent.name === node;
+      return parent.name === node;
     case 'TSQualifiedName':
-        return parent.right === node || parent.left === node;
+      return parent.right === node || parent.left === node;
     case 'TSModuleDeclaration':
     case 'TSTypeAliasDeclaration':
     case 'TSInterfaceDeclaration':
-        return parent.id === node;
+      return parent.id === node;
     case 'TSTypeReference':
-        return parent.typeName === node;
+      return parent.typeName === node;
     case 'MemberExpression':
-        return parent.property === node;
+      return parent.property === node;
     case 'ImportSpecifier':
-        return parent.imported === node;
+      return parent.imported === node;
     case 'ExportSpecifier':
-        return parent.exported === node;
+      return parent.exported === node;
     case 'TSTypePredicate':
-        return parent.parameterName === node;
+      return parent.parameterName === node;
     case 'TSFunctionType':
     case 'TSCallSignatureDeclaration':
-        return parent.params.includes(node);
+      return parent.params.includes(node);
     case 'TSIndexSignature':
-        return parent.parameters.includes(node);
+      return parent.parameters.includes(node);
     case 'TSInterfaceHeritage':
     case 'TSClassImplements':
-        return parent.expression === node;
+      return parent.expression === node;
     case 'RestElement':
-        switch (parent.parent && parent.parent.type) {
+      switch (parent.parent && parent.parent.type) {
         case 'TSCallSignatureDeclaration':
         case 'TSFunctionType':
-            return true;
+          return true;
         default:
-            return false;
-        }
+          return false;
+      }
     default:
-        return false;
-    }
+      return false;
+  }
 };
 const getVariable = (scope, name) => {
-    if (scope) {
-        return scope.set.get(name) || getVariable(scope.upper, name);
-    }
-    return null;
+  if (scope) {
+    return scope.set.get(name) || getVariable(scope.upper, name);
+  }
+  return null;
 };
 const isLocallyDeclaredVariable = (variable) => {
-    if (!variable) {
-        return false;
-    }
-    const [definition = {}] = variable.defs;
-    switch (definition.type) {
+  if (!variable) {
+    return false;
+  }
+  const [definition = {}] = variable.defs;
+  switch (definition.type) {
     case 'ImportBinding':
     case 'Parameter':
     case 'Variable':
     case 'CatchClause':
     case 'FunctionName':
     case 'ClassName':
-        return true;
+      return true;
     default:
-        return false;
-    }
+      return false;
+  }
 };
-const isTypeVariable = (variable) => Boolean(variable && variable.isTypeVariableVariable && !variable.isValueVariable);
-const getGlobalVariableNameList = ({settings: {env = {}} = {}}) => {
-    const globalVariableNameList = [];
-    if (env.node) {
-        globalVariableNameList.push('__dirname', '__filename', 'module', 'require', 'import');
-    }
-    return globalVariableNameList;
+const isTypeVariable = (variable) =>
+  Boolean(
+    variable && variable.isTypeVariableVariable && !variable.isValueVariable,
+  );
+const getGlobalVariableNameList = ({ settings: { env = {} } = {} }) => {
+  const globalVariableNameList = [];
+  if (env.node) {
+    globalVariableNameList.push(
+      '__dirname',
+      '__filename',
+      'module',
+      'require',
+      'import',
+    );
+  }
+  return globalVariableNameList;
 };
 
 /** @type {{'no-globals': RuleModule, 'print-filename': RuleModule}} */
 export const rules = {
-    'no-globals': {
-        meta: {
-            type: 'problem',
-            schema: [
-                {
-                    type: 'object',
-                    propeties: {
-                        allowed: {
-                            type: 'array',
-                            items: {type: 'string'},
-                        },
-                    },
-                },
-            ],
-            messages: {
-                undef: '\'{{name}}\' is not defined. ({{parentType}})',
+  'no-globals': {
+    meta: {
+      type: 'problem',
+      schema: [
+        {
+          type: 'object',
+          propeties: {
+            allowed: {
+              type: 'array',
+              items: { type: 'string' },
             },
+          },
         },
-        create: (context) => {
-            const allowedVariableNameList = new Set(context.options.reduce(
-                (concatenated, {allowed = []}) => concatenated.concat(allowed),
-                getGlobalVariableNameList(context),
-            ));
-            const isAllowedIdentifier = (node) => allowedVariableNameList.has(node.name);
-            return {
-                Identifier: (node) => {
-                    if (isNonVariableIdentifier(node) || isAllowedIdentifier(node)) {
-                        return;
-                    }
-                    const variable = getVariable(context.getScope(), node.name);
-                    if (isTypeVariable(variable)) {
-                        return;
-                    }
-                    if (!isLocallyDeclaredVariable(variable)) {
-                        context.report({
-                            node,
-                            messageId: 'undef',
-                            data: {...node, parentType: node.parent && node.parent.type},
-                        });
-                    }
-                },
-            };
-        },
+      ],
+      messages: {
+        undef: "'{{name}}' is not defined. ({{parentType}})",
+      },
     },
-    'print-filename': {
-        create: (context) => {
-            const starts = new Map();
-            return {
-                'Program': () => {
-                    const filename = context.getFilename();
-                    starts.set(filename, process.hrtime());
-                },
-                'Program:exit': () => {
-                    const filename = context.getFilename();
-                    const start = starts.get(filename);
-                    const [s, ns] = process.hrtime(start);
-                    const elapsed = s === 0
-                    ? `${(ns / 1e6).toFixed(3)}ms`
-                    : `${(s + ns / 1e6).toFixed(3)}s`;
-                    console.log(`${filename} (${elapsed})`);
-                },
-            };
+    create: (context) => {
+      const allowedVariableNameList = new Set(
+        context.options.reduce(
+          (concatenated, { allowed = [] }) => concatenated.concat(allowed),
+          getGlobalVariableNameList(context),
+        ),
+      );
+      const isAllowedIdentifier = (node) =>
+        allowedVariableNameList.has(node.name);
+      return {
+        Identifier: (node) => {
+          if (isNonVariableIdentifier(node) || isAllowedIdentifier(node)) {
+            return;
+          }
+          const variable = getVariable(context.getScope(), node.name);
+          if (isTypeVariable(variable)) {
+            return;
+          }
+          if (!isLocallyDeclaredVariable(variable)) {
+            context.report({
+              node,
+              messageId: 'undef',
+              data: { ...node, parentType: node.parent && node.parent.type },
+            });
+          }
         },
+      };
     },
+  },
+  'print-filename': {
+    create: (context) => {
+      const starts = new Map();
+      return {
+        Program: () => {
+          const filename = context.getFilename();
+          starts.set(filename, process.hrtime());
+        },
+        'Program:exit': () => {
+          const filename = context.getFilename();
+          const start = starts.get(filename);
+          const [s, ns] = process.hrtime(start);
+          const elapsed =
+            s === 0
+              ? `${(ns / 1e6).toFixed(3)}ms`
+              : `${(s + ns / 1e6).toFixed(3)}s`;
+          console.log(`${filename} (${elapsed})`);
+        },
+      };
+    },
+  },
 };
--- test/no-globals.test.mjs
+++ test/no-globals.test.mjs
@@ -1,73 +1,75 @@
 //@ts-check
-import {RuleTester} from 'eslint';
-import {createRequire} from 'node:module';
-import {rules as mjsRules} from '../index.mjs';
-import {rules as cjsRules} from '../index.cjs';
+import { RuleTester } from 'eslint';
+import { createRequire } from 'node:module';
+import { rules as mjsRules } from '../index.mjs';
+import { rules as cjsRules } from '../index.cjs';
 
 const require = createRequire(import.meta.url);
 const tester = new RuleTester({
-    parser: require.resolve('@typescript-eslint/parser'),
+  parser: require.resolve('@typescript-eslint/parser'),
 });
 /** @type {Array<RuleTester.ValidTestCase>} */
 const valid = [
-    {code: '1 + 1'},
-    {code: 'undefined'},
-    {code: 'null'},
-    {code: 'NaN'},
-    {code: 'true'},
-    {code: 'false'},
-    {code: 'Object.assign()', options: [{allowed: ['Object']}]},
-    {code: 'interface Foo {};class Bar implements Foo {}'},
-    {code: 'const foo: Bar = 0'},
-    {code: 'const foo: Foo.Bar = 0'},
-    {code: [
-        'class Foo {',
-        '    protected readonly foo: string',
-        '    protected constructor() {',
-        '        this.foo = "";',
-        '    }',
-        '}',
-    ].join('\n')},
-    {
-        code: 'module.parent',
-        settings: {env: {node: true}},
-    },
-    {
-        code: 'require()',
-        settings: {env: {node: true}},
-    },
-    {
-        code: 'require(__dirname, __filename)',
-        settings: {env: {node: true}},
-    },
+  { code: '1 + 1' },
+  { code: 'undefined' },
+  { code: 'null' },
+  { code: 'NaN' },
+  { code: 'true' },
+  { code: 'false' },
+  { code: 'Object.assign()', options: [{ allowed: ['Object'] }] },
+  { code: 'interface Foo {};class Bar implements Foo {}' },
+  { code: 'const foo: Bar = 0' },
+  { code: 'const foo: Foo.Bar = 0' },
+  {
+    code: [
+      'class Foo {',
+      '    protected readonly foo: string',
+      '    protected constructor() {',
+      '        this.foo = "";',
+      '    }',
+      '}',
+    ].join('\n'),
+  },
+  {
+    code: 'module.parent',
+    settings: { env: { node: true } },
+  },
+  {
+    code: 'require()',
+    settings: { env: { node: true } },
+  },
+  {
+    code: 'require(__dirname, __filename)',
+    settings: { env: { node: true } },
+  },
 ];
 /** @type {Array<RuleTester.InvalidTestCase>} */
 const invalid = [
-    {
-        code: 'module.parent',
-        errors: ['\'module\' is not defined. (MemberExpression)'],
-    },
-    {
-        code: 'require',
-        errors: ['\'require\' is not defined. (ExpressionStatement)'],
-    },
-    {
-        code: 'require(__dirname, __filename)',
-        errors: [
-            '\'require\' is not defined. (CallExpression)',
-            '\'__dirname\' is not defined. (CallExpression)',
-            '\'__filename\' is not defined. (CallExpression)',
-        ],
-    },
-    {
-        code: 'process.exit(1)',
-        errors: ['\'process\' is not defined. (MemberExpression)'],
-    },
-    {
-        code: 'Object.assign()',
-        errors: ['\'Object\' is not defined. (MemberExpression)'],
-    },
+  {
+    code: 'module.parent',
+    errors: ["'module' is not defined. (MemberExpression)"],
+  },
+  {
+    code: 'require',
+    errors: ["'require' is not defined. (ExpressionStatement)"],
+  },
+  {
+    code: 'require(__dirname, __filename)',
+    errors: [
+      "'require' is not defined. (CallExpression)",
+      "'__dirname' is not defined. (CallExpression)",
+      "'__filename' is not defined. (CallExpression)",
+    ],
+  },
+  {
+    code: 'process.exit(1)',
+    errors: ["'process' is not defined. (MemberExpression)"],
+  },
+  {
+    code: 'Object.assign()',
+    errors: ["'Object' is not defined. (MemberExpression)"],
+  },
 ];
-tester.run('no-globals', mjsRules['no-globals'], {valid, invalid});
-tester.run('no-globals', cjsRules['no-globals'], {valid, invalid});
+tester.run('no-globals', mjsRules['no-globals'], { valid, invalid });
+tester.run('no-globals', cjsRules['no-globals'], { valid, invalid });
 console.info('passed');
